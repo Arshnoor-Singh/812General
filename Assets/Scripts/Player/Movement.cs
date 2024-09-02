a73a8e2bd2a11cefb5651ae20a7c8548b0ba5014
Arshnoor Singh <arshnoor88@gmail.com>
2024-09-02 03:31:09 -0700

    Cinemachine camera start

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,14 +1,23 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     Vector2 movementValues;
+    Vector2 mousePosition;
+
+    public CinemachineVirtualCamera playerVirtualCamera;
     public float movementSpeed = 5f;
 
     public void IAMovement(InputAction.CallbackContext context)
     {
         movementValues = context.ReadValue<Vector2>();
+    }
+
+    public void IAMouseLook(InputAction.CallbackContext context)
+    {
+        mousePosition = context.ReadValue<Vector2>();
     }
 
     public void IAJump(InputAction.CallbackContext context)
