1ce52b42bf773fe0cfa441d8ffe54111aefeb9ec
Arshnoor Singh <arshnoor88@gmail.com>
2024-09-02 14:50:22 +0530

    Project

---
+++ Assets/Scripts/Learning/Learning01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Learning01 : MonoBehaviour
+{
+    int number1 = 15;
+    int number2 = 0;
+    float number03 = 67f;
+    string word = "stop talking";
+    bool condition = true;
+
+    private void Start()
+    {
+        DoSomeMath(45, 55);
+    }
+
+    bool DoSomeMath(int _parameter01, int _parameter02)
+    {
+        if(_parameter02 >= _parameter01)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }        
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 movementValues;
+    public float movementSpeed = 5f;
+
+    public void IAMovement(InputAction.CallbackContext context)
+    {
+        movementValues = context.ReadValue<Vector2>();
+    }
+
+    public void IAJump(InputAction.CallbackContext context)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movementValues.x * movementSpeed * Time.deltaTime, 0, movementValues.y * movementSpeed * Time.deltaTime);
+    }
+}
